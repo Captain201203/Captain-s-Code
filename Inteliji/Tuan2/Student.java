7988bdfbf2de8941eb01d6bdd423738ced7b72cb
Captain201203 <160829342+Captain201203@users.noreply.github.com>
2024-02-29 14:05:51 +0700

    Add files via upload

---
+++ Inteliji/Tuan2/Student.java
@@ -0,0 +1,23 @@
+
+public class Student {
+    private String id;
+    private String name;
+    private String add;
+
+    public  String getName(){
+        return name;
+    }
+
+
+    public String getID(){
+        return id;
+    }
+
+    public String getAdd(){
+        return add;
+    }
+
+    public static void main(String[] args){
+        System.out.println(("stop"));
+    }
+}
