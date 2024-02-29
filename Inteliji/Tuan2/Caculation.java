fff4fd43a2d9e76b54555b0d760e000d1643507a
Captain201203 <160829342+Captain201203@users.noreply.github.com>
2024-02-29 14:53:55 +0700

    Add files via upload

---
+++ Inteliji/Tuan2/Caculation.java
@@ -0,0 +1,21 @@
+public class Caculation {
+    public Caculation(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    private int a;
+    private  int b;
+
+    public int sum(){
+        return a+ b;
+    }
+
+    public int minus(){
+        return a-b;
+    }
+
+    public static void main(String[] args){
+        Caculation cal = new Caculation(5,7);
+        System.out.println((cal.sum()));
+    }
+}
